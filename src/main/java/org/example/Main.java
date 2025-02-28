acb5021c26e0a25fbc5fb2b8ad482a5cd8360b2c
rsalazar <ronal.salazarcasas@gmail.com>
2025-02-27 21:06:47 -0500

    Practicando GIT

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+        System.out.print("and!");
+        System.out.print("welcome!");
+        System.out.print("Hello");
+
+        }
+    }
